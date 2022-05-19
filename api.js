0e083090a901b1eac6e71032b5a54e759b837da6
TonioRodriguez <luisrodriguez.dwfc5@ucamp.utel.edu.mx>
2022-05-18 19:34:50 -0500

    third

--- api.js
+++ api.js
@@ -1,14 +1,41 @@
-const getWeather = async () => {
-    const url = 'https://api.waqi.info/feed/mexico/?token=4fd91d25f55408d38f12fcdc71a890c7c642592e'
-    const res = await fetch(url)
-  
-    console.log(res)
+/*https://api.apilayer.com/exchangerates_data/timeseries?apikey=C0jx7S5NBkQhZsIXq0RDCpx7lpgub1tK&base=USD&symbols=MXN&start_date=2020-03-12&end_date=2020-04-01*/
+const date1au = document.getElementById('date1au').value;
+const date2au = document.getElementById('date2au').value;
+
+const date1ag = document.getElementById('date1ag').value;
+const date2ag = document.getElementById('date2ag').value;
+
+const date1mx = document.getElementById('date1mx').value;
+const date2mx = document.getElementById('date2mx').value;
+
+
+const urlag = `https://www.metals-api.com/api/timeseries?access_key=7k1vui0duyg0c55p14zqvk11j5nf3mvwzflcgmruuu05ql74dlf7o3lsh5na&start_date=${date1ag}&end_date=${date2ag}&base=USD&symbols=XAG`
+const urlmx = `https://www.metals-api.com/api/timeseries?access_key=7k1vui0duyg0c55p14zqvk11j5nf3mvwzflcgmruuu05ql74dlf7o3lsh5na&start_date=${date1mx}&end_date=${date2mx}&base=USD&symbols=MXN`
+
+
+const getAu = async () => {
+    const urlau = `https://www.metals-api.com/api/timeseries?access_key=7k1vui0duyg0c55p14zqvk11j5nf3mvwzflcgmruuu05ql74dlf7o3lsh5na&start_date=${date1au}&end_date=${date2au}&base=USD&symbols=XAU`
+    const res = await fetch(urlau)
     const data = await res.json()
-  
     console.log(data)
-    setGraph(data.data)
   }
   
+getAu()
 
+/*const getAg = async () => {
+    const urlag = `https://www.metals-api.com/api/timeseries?access_key=7k1vui0duyg0c55p14zqvk11j5nf3mvwzflcgmruuu05ql74dlf7o3lsh5na&start_date=${date1au}&end_date=${date2au}&base=USD&symbols=XAU`
+    const resa = await fetch(urlag)
+    const dataa = await resa.json()
+    console.log(dataa)
+  }
   
-  getWeather()
+getAg()
+
+const getMx = async () => {
+    const urlmx = `https://www.metals-api.com/api/timeseries?access_key=7k1vui0duyg0c55p14zqvk11j5nf3mvwzflcgmruuu05ql74dlf7o3lsh5na&start_date=${date1au}&end_date=${date2au}&base=USD&symbols=XAU`
+    const resm = await fetch(urlmx)
+    const datam = await resm.json()
+    console.log(datam)
+  }
+  
+getMx()*/
