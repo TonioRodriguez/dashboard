2f9ea535861768ee18f33b0547d282f17731d6d4
TonioRodriguez <luisrodriguez.dwfc5@ucamp.utel.edu.mx>
2022-05-21 19:50:29 -0500

    third

--- index.js
+++ index.js
@@ -1,34 +1,24 @@
-const setGraph = (data) => {
-    const labels = Object.keys(data.iaqi)
-    const converData = Object.values(data.iaqi).map((item) => item.v)
-    console.log(converData)
-  
-    const dataConfig = {
-      labels: labels,
-      datasets: [{
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: converData,
-      }]
-    };
-  
-  
-    const config = {
-      type: 'bar',
-      data: dataConfig,
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true
-          }
-        }
-      }
-    };
-  
-    const myChart = new Chart(
-      document.getElementById('myChart'),
-      config
-    )
-  
-  }
+const date1au = document.getElementById('date1au').value;
+const date2au = document.getElementById('date2au').value;
+
+const date1ag = document.getElementById('date1ag').value;
+const date2ag = document.getElementById('date2ag').value;
+
+const date1mx = document.getElementById('date1mx').value;
+const date2mx = document.getElementById('date2mx').value;
+
+const chartau = async () => {
+  const date1au = document.getElementById('date1au').value;
+  const date2au = document.getElementById('date2au').value;
+  const urlau = `https://www.metals-api.com/api/timeseries?access_key=7k1vui0duyg0c55p14zqvk11j5nf3mvwzflcgmruuu05ql74dlf7o3lsh5na&start_date=${date1au}&end_date=${date2au}&base=XAU&symbols=USD`
+  const res = await fetch(urlau)
+  const data = await res.json()
+  console.log(data)
+
+  grafica()
+}
+
+const grafica = ({data}) => {
+  const labels = Object.keys(rates)
+  console.log(labels)
+}
